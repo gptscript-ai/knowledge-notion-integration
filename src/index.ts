7c184b4bf84130add25464795f420ed183857f21
Daishan Peng <daishan@acorn.io>
2024-09-13 16:30:07 -0700

    Support for otto
    Signed-off-by: Daishan Peng <daishan@acorn.io>

--- src/index.ts
+++ src/index.ts
@@ -11,108 +11,133 @@ import * as fs from "node:fs";
 
 dotenv.config();
 
+interface Metadata {
+  input: InputMetadata
+  output: OutputMetadata
+}
+
+interface InputMetadata {
+  pages: string[]
+  outputDir: string
+}
+
+interface OutputMetadata {
+  files: {
+    [pageId: string]: {
+      updatedAt: string;
+      filename: string;
+      url: string;
+    }
+  }
+  pageList: {
+    [pageId: string]: {
+      url: string;
+      filename: string;
+    }
+  }
+  status: string,
+  error: string
+}
+
+// Function to write a page to a file
+async function writePageToFile(client: Client, page: PageObjectResponse, directory: string) {
+  const pageId = page.id;
+  const pageContent = await getPageContent(client, pageId);
+  const fileDir = path.join(directory, pageId.toString());
+  await mkdir(fileDir, { recursive: true });
+  const filePath = getPath(directory, page)
+  fs.writeFileSync(filePath, pageContent, "utf8");
+}
+
+function getPath(directory: string, page: PageObjectResponse): string {
+  const pageId = page.id;
+  const fileDir = path.join(directory, pageId.toString());
+  let title = ((page.properties?.title ?? page.properties?.Name) as any)?.title[0]?.plain_text?.trim().replaceAll(/\//g, "-");
+  if (!title) {
+    title = pageId.toString();
+  }
+  return path.join(fileDir, title + ".md");
+}
+
+// Function to fetch all pages
+async function fetchAllPages(client: Client) {
+  let pages: Map<string, PageObjectResponse> = new Map();
+  let cursor: string | undefined = undefined;
+
+
+  while (true) {
+    const response: SearchResponse = await client.search({
+      filter: {
+        property: "object",
+        value: "page",
+      },
+      start_cursor: cursor,
+    });
+
+    for (const page of response.results) {
+      if ((page as PageObjectResponse).archived) {
+        continue
+      }
+      pages.set(page.id, page as PageObjectResponse);
+    }
+
+    if (!response.has_more) {
+      break;
+    }
+
+    cursor = response.next_cursor ?? undefined;
+  }
+
+  return pages;
+}
+
+
 async function main() {
-  const notion = new Client({
+  const client = new Client({
     auth: process.env.NOTION_TOKEN,
   });
-
-  // Function to write a page to a file
-  async function writePageToFile(page: PageObjectResponse, directory: string) {
-    const pageId = page.id;
-    const pageContent = await getPageContent(notion, pageId);
-    const fileDir = path.join(directory, pageId.toString());
-    await mkdir(fileDir, { recursive: true });
-    const filePath = getPath(directory, page)
-    fs.writeFileSync(filePath, pageContent, "utf8");
-  }
-
-  function getPath(directory: string, page: PageObjectResponse): string {
-    const pageId = page.id;
-    const fileDir = path.join(directory, pageId.toString());
-    let title = ((page.properties?.title ?? page.properties?.Name) as any)?.title[0]?.plain_text?.trim().replaceAll(/\//g, "-");
-    if (!title) {
-      title = pageId.toString();
-    }
-    return path.join(fileDir, title + ".md");
-  }
-
-  // Function to fetch all pages
-  async function fetchAllPages() {
-    let pages: any[] = [];
-    let cursor: string | undefined = undefined;
-
-
-    while (true) {
-      const response: SearchResponse = await notion.search({
-        filter: {
-          property: "object",
-          value: "page",
-        },
-        start_cursor: cursor,
-      });
-
-      pages = pages.concat(response.results);
-
-      if (!response.has_more) {
-        break;
-      }
-
-      cursor = response.next_cursor ?? undefined;
-    }
-
-    return pages;
-  }
+  let workingDir = process.env.GPTSCRIPTS_WORKSPACE_DIR!!;
 
   // Fetch all pages
-  const pages = await fetchAllPages();
-  let metadata: Map<string, {
-    url: string;
-    filename: string
-    updatedAt: string;
-    sync: boolean;
-  }> = new Map();
-  const outputDir = path.join(process.env.WORKSPACE_DIR!!, 'knowledge', 'integrations', 'notion');
-  await mkdir(outputDir, { recursive: true });
-  const metadataPath = path.join(outputDir, 'metadata.json');
+  const pages = await fetchAllPages(client);
+  let metadata: Metadata = {} as Metadata
+  const metadataPath = path.join(workingDir, '.metadata.json');
   if (fs.existsSync(metadataPath)) {
-    metadata = new Map(Object.entries(JSON.parse(fs.readFileSync(metadataPath, 'utf8').toString())));
+    metadata = JSON.parse(fs.readFileSync(metadataPath, 'utf8').toString());
+  }
+  if (metadata.input.outputDir) {
+    workingDir = metadata.input.outputDir
   }
 
-  let updatedPages = 0;
-  for (const page of pages) {
-    if (metadata.has(page.id)) {
-      const entry = metadata.get(page.id)
-      if (entry?.updatedAt === page.last_edited_time && fs.existsSync(getPath(outputDir, page))) {
-        continue;
-      }
-      if (entry?.sync) {
-        updatedPages++
-        await writePageToFile(page, outputDir);
-      }
-      metadata.set(page.id, {
-        url: page.url,
-        filename: path.basename(getPath(outputDir, page)),
-        updatedAt: page.last_edited_time,
-        sync: entry?.sync ?? false,
-      })
-    }
-    metadata.set(page.id, {
+  if (!metadata.output.pageList) {
+    metadata.output.pageList = {}
+  }
+
+  for (const [pageId, page] of pages.entries()) {
+    metadata.output.pageList[pageId] = {
       url: page.url,
-      filename: path.basename(getPath(outputDir, page)),
-      updatedAt: page.last_edited_time,
-      sync: false,
-    })
+      filename: path.basename(getPath(workingDir, page)),
+    };
   }
-
-  for (const [key, _] of metadata) {
-    if (!pages.find((page) => page.id === key)) {
-      fs.rmSync(path.join(outputDir, key), { recursive: true });
-      console.log(`Removed ${key} from ${outputDir}`);
-      metadata.delete(key);
+  let syncedCount = 0;
+  try {
+    for (const pageId of metadata.input.pages) {
+      if (pages.has(pageId)) {
+        await writePageToFile(client, pages.get(pageId)!, workingDir);
+        syncedCount++;
+        metadata.output.pageList[pageId] = {
+          url: pages.get(pageId)!.url,
+          filename: path.basename(getPath(workingDir, pages.get(pageId)!)),
+        };
+      }
     }
+    metadata.output.status = `${syncedCount} number of pages have been synced`;
+  } catch (error: any) {
+    metadata.output.error = error.message;
+    throw error;
+  } finally {
+    await writeFile(metadataPath, JSON.stringify(metadata, 'utf8'));
   }
-
-  await writeFile(metadataPath, JSON.stringify(Object.fromEntries(metadata)), 'utf8');
 
   console.log(`Finished writing ${updatedPages} pages to ${outputDir}`);
 }
